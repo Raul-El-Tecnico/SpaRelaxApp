a3f21dfb092c9a906f5a24891ac0da21b8665072
Gabriela CM <gabrielacaligari@hotmail.com>
2026-08-25 15:59:19 +0200

    uppdaterat texten och fixat vissa fel

---
+++ Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaRelaxApp.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ServiceType { get; set; } = string.Empty; // T.ex. Massage, Ansiktsbehandling
+
+        [Required]
+        public DateTime BookingDate { get; set; }
+
+        // Koppling till användaren
+        public string UserId { get; set; } = string.Empty;
+    }
+}
